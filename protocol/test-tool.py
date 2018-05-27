1e20d2e38e90079c9c1a35cca08f997e87faee2d
Nick Ames <nick@fetchmodus.org>
2018-05-27 05:57:28 -0700

    ljwerlkjel

--- protocol/test-tool.py
+++ protocol/test-tool.py
@@ -14,14 +14,35 @@ SoftwareDir=os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file_
 sys.path.append(os.path.abspath(SoftwareDir) + "/python-library/")
 import robolib
 
+def minmax_values(size_code):
+	"""Given a size code, return the (min, max) values as a tuple."""
+	return {"accum"    : (-2**15, 2**15 - 1),
+			"int8_t"   : (-2**7,2**7 - 1),
+			"uint8_t"  : (0,2**8),
+			"int16_t"  : (-2**15, 2**15 - 1),
+			"uint16_t" : (0,2**16),
+			"int32_t"  : (-2**31,2**31 - 1),
+			"uint32_t" : (0,2**32)}[size_code]
+
 def setup_controls(window, protocol):
+	"""Setup the window controls."""
 	window.setWindowTitle("Test Tool")
 	ww = QWidget(window)
 	flayout = QFormLayout(ww)
 	for r in protocol:
 		label = QLabel(r.name)
 		hl = QHBoxLayout()
-		
+		if r.size == "accum":
+			sp = QDoubleSpinBox()
+		else:
+			sp = QSpinBox()
+		minval, maxval = minmax_values(r.size)
+		sp.setMinimum(minval)
+		sp.setMaximum(maxval)
+		sp.setValue(r.default)
+		hl.addWidget(sp)
+		#hl.addWidget(readbtn)
+		#hl.addWidget(writebtn)
 		flayout.addRow(label, hl)
 	ww.setLayout(flayout)
 	window.setCentralWidget(ww)
