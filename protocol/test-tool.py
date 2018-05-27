1f006fe117ff637661f75c2d95db2b2f78d980f4
Nick Ames <nick@fetchmodus.org>
2018-05-27 03:43:35 -0700

    sdjlsdfjsldkfj

---
+++ protocol/test-tool.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#Junior Design Robot
+#Test tool
+#Nick Ames 2018
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import signal
+import os
+import sys
+
+SoftwareDir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)) + os.sep
+sys.path.append(os.path.abspath(SoftwareDir) + "../python-library/")
+import gen_files
+import robolib.py
+
+def main():
+	signal.signal(signal.SIGINT, signal.SIG_DFL) #Make Ctrl-C quit the program
+	print("test")
+	
+if __name__ == "__main__":
+	main()
+	
